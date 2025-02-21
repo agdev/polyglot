df1bd862065a8fcb37b03d73759841ead0365dd7
AG <topexpertsolutions@gmail.com>
2025-02-21 16:20:56 +0200

    clean up

--- streamlit_app/words_page.py
+++ streamlit_app/words_page.py
@@ -4,9 +4,5 @@ from util import display_filtered_dataframe
 if ('word_df' in st.session_state) and (st.session_state.word_df is not None):
     # Display both dataframes
     display_filtered_dataframe(st.session_state.word_df, "Words", ['Translation_Language', 'Source_Language'])
-    st.markdown("---")
-    if st.button("Download Vocabulary"):
-        # TODO: Implement download functionality
-        st.text_input("Enter your email:", key="download_email")
 else:
     st.write("No words yet, go chat with your AI Assistant!")
